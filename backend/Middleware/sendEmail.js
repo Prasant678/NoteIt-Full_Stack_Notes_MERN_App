30c6659ba6f85c79cecdef6f55b8daad4a33d13a
G Prasant <prasangtrao917@gmail.com>
2026-02-25 21:32:49 +0530

    Change in sendEmail Code

--- backend/Middleware/sendEmail.js
+++ backend/Middleware/sendEmail.js
@@ -3,10 +3,15 @@ import nodeMailer from 'nodemailer';
 const sendEmail = async (to, subject, html) => {
     try {
         const transport = nodeMailer.createTransport({
-            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
+            },
+            tls: {
+                rejectUnauthorized: false
             }
         })
 
